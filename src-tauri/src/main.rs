580b29f74e2c5c5fdc0b7af0a14a59760581bdff
dafen173 <dafen@ukr.net>
2025-06-24 22:30:08 +0300

    fix: OUT_DIR env var is not set, do you have a build script?

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// fn main() {
+//     my_app_lib::run()
+// }
+
+
+
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    my_app_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
 }
